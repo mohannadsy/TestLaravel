2fc7d2e4a144f8a0b9f6b8dd9061e08084d8fc60
mohannad-mahmoud <mohannadsy96@gmail.com>
2022-05-29 14:55:35 +0300

    First Upload Project

---
+++ resources/js/app.js
@@ -0,0 +1,18 @@
+require('./bootstrap');
+
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/inertia-vue3';
+import { InertiaProgress } from '@inertiajs/progress';
+
+createInertiaApp({
+    title: (title) => `${title}`,
+    resolve: (name) => require(`./Pages/${name}.vue`),
+    setup({ el, app, props, plugin }) {
+        return createApp({ render: () => h(app, props) })
+            .use(plugin)
+            .mixin({ methods: { route } })
+            .mount(el);
+    },
+});
+
+InertiaProgress.init({ color: '#4B5563' });
