21a97b1a95d6c0fdae7f6afe3759621b1691fb97
rgdrgd <raghad.nanou@gmail.com>
2022-05-31 12:13:18 +0300

    switched to bootstrap

--- resources/js/Jetstream/Label.vue
+++ resources/js/Jetstream/Label.vue
@@ -1,12 +1,14 @@
-<script setup>
-defineProps({
-    value: String,
-});
-</script>
-
 <template>
-    <label class="block font-medium text-sm text-gray-700">
-        <span v-if="value">{{ value }}</span>
-        <span v-else><slot /></span>
-    </label>
+	<label :class="classes">
+		<span v-if="value">{{ value }}</span>
+		<span v-else><slot></slot></span>
+	</label>
 </template>
+
+<script>
+  import { defineComponent } from 'vue'
+
+	export default defineComponent({
+		props: ['value', 'classes']
+	})
+</script>
--- resources/js/Pages/PrivacyPolicy.vue
+++ resources/js/Pages/PrivacyPolicy.vue
@@ -1,24 +1,30 @@
-<script setup>
-import { Head } from '@inertiajs/inertia-vue3';
-import JetAuthenticationCardLogo from '@/Jetstream/AuthenticationCardLogo.vue';
-
-defineProps({
-    policy: String,
-});
-</script>
-
 <template>
-    <Head title="Privacy Policy" />
-
-    <div class="font-sans text-gray-900 antialiased">
-        <div class="pt-4 bg-gray-100">
-            <div class="min-h-screen flex flex-col items-center pt-6 sm:pt-0">
-                <div>
-                    <JetAuthenticationCardLogo />
-                </div>
-
-                <div class="w-full sm:max-w-2xl mt-6 p-6 bg-white shadow-md overflow-hidden sm:rounded-lg prose" v-html="policy" />
-            </div>
+  <Head title="Privacy Policy" />
+    <div class="row justify-content-center pt-4">
+      <div class="col-6">
+        <div>
+          <jet-authentication-card-logo />
         </div>
+
+        <div class="card shadow-sm">
+          <div v-html="policy" class="card-body">
+          </div>
+        </div>
+      </div>
     </div>
 </template>
+
+<script>
+import { defineComponent } from "vue"
+import { Head } from '@inertiajs/inertia-vue3'
+import JetAuthenticationCardLogo from '@/Jetstream/AuthenticationCardLogo.vue'
+
+export default defineComponent({
+  props: ['policy'],
+
+  components: {
+    Head,
+    JetAuthenticationCardLogo,
+  },
+})
+</script>
