f47187929c30b3d774d7ef9317a61ba8b98527e9
Nour Alkinj <noor22w2@gmail.com>
2022-05-31 15:14:50 +0300

    update by nour

--- database/migrations/2022_05_29_123207_create_branches_table.php
+++ database/migrations/2022_05_29_123207_create_branches_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('branches', function (Blueprint $table) {
             $table->id()->unique();
+            $table->string('code') ;
             $table->string('name') ;
             $table->unsignedBigInteger('branch_id')->nullable();
             $table->string('responsibility')->nullable();
