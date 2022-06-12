53c8c817996eca1c4328959a8236acb8720ddb71
Nour Alkinj <noor22w2@gmail.com>
2022-06-12 13:40:12 +0300

    up by nour

--- routes/web.php
+++ routes/web.php
@@ -96,6 +96,7 @@ Route::group(['namespace' => 'Branch', 'prefix' => 'branch'], function () {
     Route::get('edit/{id}', [\App\Http\Controllers\Branch\BranchController::class, 'edit'])->name('branch.edit');
     Route::post('update/{id}', [\App\Http\Controllers\Branch\BranchController::class, 'update'])->name('branch.update');
     Route::get('delete/{id}', [\App\Http\Controllers\Branch\BranchController::class, 'delete'])->name('branch.delete');
+    Route::get('destroy/{id}', [\App\Http\Controllers\Branch\BranchController::class, 'destroy'])->name('branch.destroy');
     Route::get('isLastCharacterInStringIsNumeric/{string}', [\App\Http\Controllers\Branch\BranchController::class, 'isLastCharacterInStringIsNumeric'])->name('branch.isLastCharacterInStringIsNumeric');
 
 
