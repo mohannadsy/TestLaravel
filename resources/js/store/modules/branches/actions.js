6c985484c75a4b335e15d5a60c63be8b1808b0f6
SaraAbdo96 <sara.nadim.abdo@gmail.com>
2022-06-14 15:41:24 -0700

    branch

--- resources/js/store/modules/branches/actions.js
+++ resources/js/store/modules/branches/actions.js
@@ -1,3 +1,17 @@
 export default {
-
+    registerBranch(context, data) {
+        const branchData = {
+            //id: context.rootGetters.userId,
+            code: data.code,
+            name: data.name,
+            branch_id: data.branch_id,
+            responsibility: data.responsibility,
+            address: data.address,
+            website: data.website,
+            email: data.email,
+            phone: data.phone,
+            mobile: data.mobile
+        };
+        context.commit('registerBranch', branchData);
+    }
 };
