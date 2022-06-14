6e308ec69ed9e1ad0b3ba23efe7127720b8e372d
SaraAbdo96 <sara.nadim.abdo@gmail.com>
2022-06-14 00:26:40 -0700

    update vuex

--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -1,8 +1,12 @@
 import { createStore } from "vuex";
 
+import branchesModule from './modules/branches/index.js';
+import usersModule from './modules/users/index.js';
+
 const store = createStore({
     modules: {
-
+        branches: branchesModule,
+        users: usersModule
     }
 });
 
