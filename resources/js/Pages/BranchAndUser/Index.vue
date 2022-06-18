b233255d293befb7c4c875eb4c937912c57722df
SaraAbdo96 <sara.nadim.abdo@gmail.com>
2022-06-18 15:50:03 -0700

    get branches

--- resources/js/Pages/BranchAndUser/Index.vue
+++ resources/js/Pages/BranchAndUser/Index.vue
@@ -3,19 +3,25 @@
         <div class="col-6">
             <branch-form @save-data="saveData"></branch-form>
         </div>
+        <button @click="loadBranches">Get</button>
     </div>
 </template>
 
 <script>
 import BranchForm from '../../components/branches/BranchForm.vue';
+import Button from '../../Jetstream/Button.vue';
 export default {
     props:['code','name'],
     components:{
-        BranchForm
+        BranchForm,
+        Button
     },
     methods:{
         saveData(data){
             this.$store.dispatch('branches/registerBranch',data);
+        },
+        loadBranches(){
+            this.$store.dispatch('branches/loadBranches');
         }
     }
 }
