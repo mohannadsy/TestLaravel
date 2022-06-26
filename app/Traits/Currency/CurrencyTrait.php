6c1537fed731e55fe065fbdcf1951ba5ebec2f2e
Clauda <klodaalrakkad@gmail.com>
2022-06-26 11:17:10 +0300

    clauda

--- app/Traits/Currency/CurrencyTrait.php
+++ app/Traits/Currency/CurrencyTrait.php
@@ -5,14 +5,14 @@ namespace App\Traits\Currency;
 trait  CurrencyTrait {
 
 
-    public function isDefaultAccuracy($id)
+    public function isDefaultCurrency($id)
     {
         return $id == 1;
     }
 
-    public function isNotDefaultAccuracy($id)
+    public function isNotDefaultCurrency($id)
     {
-        return !$this->isDefaultAccuracy($id);
+        return !$this->isDefaultCurrency($id);
     }
 
 }
