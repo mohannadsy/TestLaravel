792e7b8ce34b382fe991c0fb89bccb5818ed9c36
Clauda <klodaalrakkad@gmail.com>
2022-07-04 12:40:48 +0300

    back

--- database/migrations/2022_07_04_060409_create_permission_groups_table.php
+++ database/migrations/2022_07_04_060409_create_permission_groups_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('permission_groups', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('caption');
+            $table->json('caption');
             $table->timestamps();
         });
     }
