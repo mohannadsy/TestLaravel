024defdcd42dc83df811ec0d78ff37da25f2ebff
rgdrgd <raghad.nanou@gmail.com>
2022-07-13 12:51:12 +0300

    rgd

--- resources/js/components/users/Index.vue
+++ resources/js/components/users/Index.vue
@@ -76,7 +76,7 @@ export default {
   },
   data() {
     return {
-      activeTab: "BasicInformation",
+      activeTab: "Permissions",
        postData:reactive({
 
         code:'',
--- resources/js/components/users/Permissions.vue
+++ resources/js/components/users/Permissions.vue
@@ -1,20 +1,23 @@
 <template>
-<div>
-    <!-- <ul v-for="(groupPermission, i) in groupPermissions" :key="i" >
+    <div>
+        <!-- <ul v-for="(groupPermission, i) in groupPermissions" :key="i" >
       <elemet-label> {{groupPermission.caption}}</elemet-label>
       <li v-for="(permission,i) in groupPermission.permissions" :key="i">
                 {{ permission}}
       </li>
     </ul> -->
-     <ul >
-      <elemet-label> raghad</elemet-label>
-      <li >
-            <elemet-label>hello</elemet-label>
-      </li>
-    </ul>
-</div>
-    </template>
-
+        <ul class="main" v-for="(group,i) in groups" :key="i" >
+            <elemet-label> حسابات</elemet-label>
+            <ul>
+                <!-- <elemet-label>{{group}} </elemet-label> -->
+                <li class="col-3" v-for="(permission,i) in permissions" :key="i">
+                <checkbox-switch>   </checkbox-switch>
+                {{permission}}
+               </li>
+            </ul>
+        </ul>
+    </div>
+</template>
 
 <script>
 import ElemetLabel from "../../Shared/ElemetLabel.vue";
@@ -23,26 +26,48 @@ import CheckboxSwitch from "../../Shared/CheckboxSwitch.vue";
 
 export default {
     components: {
-    ElemetLabel,
-    ElementCheckbox,
-    CheckboxSwitch,
-
-},
-props:{
-    groupPermissions:Array,
-},
+        ElemetLabel,
+        ElementCheckbox,
+        CheckboxSwitch,
+    },
+    props: {
+        // groupPermissions:Array,
+    },
 
     data() {
+        return {
+            groups:['دليل الحسابات','بطاقة حساب'],
+            permissions:['إضافة بطاقة حساب','تعديل بطاقة ','حذف','استعراض','حذف نهائي','استعادة','طباعة','طباعة','حذف',]
 
+        };
     },
 };
-
 </script>
 
 <style scoped>
-table,th,td{
+table,
+th,
+td {
     border: 1px solid;
-
+}
+.main{
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
 }
 
+
+li
+{
+  float :right;
+  display: block;
+  text-align: center;
+  padding: 20px 16px ;
+  text-decoration: none;
+}
+
+li a:hover {
+  background-color: #111111;
+}
 </style>
