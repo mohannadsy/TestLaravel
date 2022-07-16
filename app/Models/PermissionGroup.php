3fc4c4258294e094d8c1d8de0a3b2e26a8418b0d
Clauda <klodaalrakkad@gmail.com>
2022-07-16 15:26:17 +0300

    back

--- app/Models/PermissionGroup.php
+++ app/Models/PermissionGroup.php
@@ -11,12 +11,12 @@ class PermissionGroup extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name', 'caption_ar','caption_en'];
-    protected $hidden = ['created_at', 'updated_at','id'];
+    protected $fillable = ['name', 'caption_ar', 'caption_en'];
+    protected $hidden = ['created_at', 'updated_at', 'id'];
 
     public function permissions()
     {
-        return $this->hasMany(Permission::class, 'permission_group_id')->select('caption_'.Config::get('app.locale').' as caption ','permission_group_id');
+        return $this->hasMany(Permission::class, 'permission_group_id')->select('caption_' . Config::get('app.locale') . ' as caption ', 'permission_group_id');
     }
 
 
