ea73b89f97744bfecf9e25fb573350754997a987
Nour Alkinj <noor22w2@gmail.com>
2022-07-16 12:09:49 +0300

    nour

--- app/Models/PermissionGroup.php
+++ app/Models/PermissionGroup.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Config;
 use Spatie\Permission\Models\Permission;
 
 class PermissionGroup extends Model
@@ -15,9 +16,9 @@ class PermissionGroup extends Model
 
     public function permissions()
     {
-        return $this->hasMany(Permission::class, 'permission_group_id');
-
+        return $this->hasMany(Permission::class, 'permission_group_id')->select('caption_'.Config::get('app.locale').' as caption ','permission_group_id');
     }
+
 
 
 //    protected $casts = [
