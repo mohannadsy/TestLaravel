5fd72f1809fc1834216ad4500bb4323754fd7e8e
Clauda <klodaalrakkad@gmail.com>
2022-07-18 13:00:23 +0300

    back

--- app/Traits/User/UserTrait.php
+++ app/Traits/User/UserTrait.php
@@ -97,8 +97,8 @@ trait  UserTrait
                 }
             }
         }
-
-        return Inertia::render('BranchAndUser/Index', compact('groupPermissions', 'user'));
+        return $groupPermissions;
+//        return Inertia::render('BranchAndUser/Index', compact('groupPermissions', 'user'));
     }
 
     public function rolePermission($roleId)
@@ -115,9 +115,9 @@ trait  UserTrait
                 }
             }
         }
-        //return $groupPermissions;
+        return $groupPermissions;
 
-       return Inertia::render('BranchAndUser/Index', compact('groupPermissions', 'role'));
+//       return Inertia::render('BranchAndUser/Index', compact('groupPermissions', 'role'));
     }
 
 //    public function userPermissionTow($userId)
