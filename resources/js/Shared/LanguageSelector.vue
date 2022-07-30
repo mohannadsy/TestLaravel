039595fcf9d3ae8c83b54435b10703b557d4927c
Nour Alkinj <noor22w2@gmail.com>
2022-07-30 23:35:23 +0300

    nour

---
+++ resources/js/Shared/LanguageSelector.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="ml-4">
+        <inertia-link :href="route('language', [selectable_locale])">
+            <icon class="w-5 h-5" :name="selectable_locale" />
+        </inertia-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "LanguageSelector"
+    }
+</script>
+
+<style scoped>
+
+</style>
