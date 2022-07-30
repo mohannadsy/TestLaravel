2cb6ce1f5f40e39b72406110257de4a45ad9c7c2
Nour Alkinj <noor22w2@gmail.com>
2022-07-30 12:12:18 +0300

    nour

--- app/Http/Controllers/Branch/BranchController.php
+++ app/Http/Controllers/Branch/BranchController.php
@@ -101,78 +101,94 @@ class BranchController extends Controller
         } else
             return "it is not possible to delete a branch that contains branches within it";
     }
+//    public function store(Request $request)
+//    {
+//        $rules = $this -> getRules();
+//        $messages= $this->getMessages();
+//        $validator= Validator::make($request->all(),$rules,$messages);
+//        if($validator->fails())
+//        {
+//            return redirect()->back()->withErrors($validator)->withInputs($request->all());
+//        }
+//         Branch::create([
+//             'code'                   =>$request -> code ,
+//              'name'                   =>$request -> name ,
+//              'branch_id'              =>$request -> branch_id ,
+//              'responsibility'          =>$request -> responsibility ,
+//              'address'                =>$request -> address ,
+//              'website'              =>$request -> website ,
+//              'email'                  =>$request -> email ,
+//              'phone'                 =>$request -> phone ,
+//              'mobile'                =>$request -> mobile ,
+//         ]);
+//        $id = Branch::orderBy('id', 'desc')->first()->id + 1;
+//        $parameters = ['request' => $request, 'id' => $id];
+//        $data= 'successfully' ;
+//        $this->callActivityMethod('store', $parameters);
+//        return $data;
+////             return Inertia::render('BranchAndUser/Index',compact('data'));
+//    }
+//    protected function getMessages()
+//    {
+//        return $messages =[
+//            'code.required'          => 'رمز الفرع يجب أن يُدخل',
+//            'code.unique'            => 'رمز الفرع موجود سابقاً ',
+//            'code.string'            => 'رمز الفرع يجب أن يكون محارف ',
+//            'name.required'          => 'اسم الفرع يجب أن يُدخل',
+//            'name.max'               => 'اسم الفرع تجاوز 50 محرف ',
+//            'name.unique'            => 'اسم الفرع موجود سابقاً ',
+//            'name.string'            => 'اسم الفرع يجب أن يكون محارف ',
+//            'branch_id.max'          => 'رمز الفرع الرئيسي تجاوز 50 رقم ',
+//            'branch_id.numeric'      => 'رمز الفرع الرئيسي يجب أن يكون ارقام  ',
+//            'responsibility.max'     => 'معلومات الفرع(الصفة) تجاوز 250 محرف ',
+//            'responsibility.string'  => 'معلومات الفرع يجب أن يكون محارف ',
+//            'address.max'            =>  'عنوان الفرع تجاوز 100 محرف ',
+//            'address.string'         =>'عنوان الفرع يجب أن يكون محارف ',
+//            'website.unique'         =>'الموقع الالكتروني مُدخل سابقاً لفرع آخر ',
+//            'website.string'         =>'الموقع الالكتروني يجب أن يكون محارف  ',
+//            'email.string'           =>' البريد الالكتروني  يجب أن يكون محارف ',
+//            'email.email'            =>'ماتم إدخاله غير متوافق مع صيغة البريد الإلكتروني  ',
+//            'email.unique'           =>'البريد الالكتروني مُدخل سابقاً لفرع آخر  ',
+//            'phone.max'              => 'رقم الهاتف تجاوز 50 رقم ',
+//            'phone.unique'           => 'رقم الهاتف مُدخل سابقاً لفرع آخر ',
+//            'phone.string'           =>'رقم الهاتف يجب أن يكون محارف ',
+//            'mobile.max'             => 'رقم الموبايل تجاوز 50 محرف ',
+//            'mobile.unique'          => 'رقم الموبايل مُدخل سابقاً لفرع آخر ',
+//            'mobile.string'          =>'رقم الموبايل يجب أن يكون محارف ',
+//
+//        ];
+//    }
+
+//    protected function getRules()
+//    {
+//        return $rules = [
+//
+//              'code'                   =>'required|string|unique:branches,code',
+//              'name'                   =>'required|max:50|string|unique:branches,name' ,
+//              'branch_id'              =>'max:50|numeric' ,
+//              'responsibility'         =>'max:250|string',
+//              'address'                =>'max:100|string',
+//              'website'                =>'string|unique:branches,website',
+//              'email'                  =>'string|email|unique:branches,email',
+//              'phone'                  =>'max:50|string|unique:branches,phone',
+//              'mobile'                 =>'max:50|string|unique:branches,mobile',
+//        ];
+//
+//    }
+
     public function store(Request $request)
     {
-        $rules = $this -> getRules();
-        $messages= $this->getMessages();
-        $validator= Validator::make($request->all(),$rules,$messages);
-        if($validator->fails())
-        {
-            return redirect()->back()->withErrors($validator)->withInputs($request->all());
-        }
          Branch::create([
-             'code'                   =>$request -> code ,
-              'name'                   =>$request -> name ,
-              'branch_id'              =>$request -> branch_id ,
-              'responsibility'          =>$request -> responsibility ,
-              'address'                =>$request -> address ,
-              'website'              =>$request -> website ,
-              'email'                  =>$request -> email ,
-              'phone'                 =>$request -> phone ,
-              'mobile'                =>$request -> mobile ,
+             'code'               =>$request->code ,
+              'name'              =>$request->name ,
+              'branch_id'         =>$request->branch_id ,
+              'responsibility'    =>$request->responsibility ,
+              'address'           =>$request->address ,
+              'website'           =>$request->website ,
+              'email'             =>$request->email ,
+              'phone'             =>$request->phone ,
+              'mobile'            =>$request->mobile ,
          ]);
-        $id = Branch::orderBy('id', 'desc')->first()->id + 1;
-        $parameters = ['request' => $request, 'id' => $id];
-        $data= 'successfully' ;
-        $this->callActivityMethod('store', $parameters);
-        return $data;
-//             return Inertia::render('BranchAndUser/Index',compact('data'));
-    }
-    protected function getMessages()
-    {
-        return $messages =[
-            'code.required'          => 'رمز الفرع يجب أن يُدخل',
-            'code.unique'            => 'رمز الفرع موجود سابقاً ',
-            'code.string'            => 'رمز الفرع يجب أن يكون محارف ',
-            'name.required'          => 'اسم الفرع يجب أن يُدخل',
-            'name.max'               => 'اسم الفرع تجاوز 50 محرف ',
-            'name.unique'            => 'اسم الفرع موجود سابقاً ',
-            'name.string'            => 'اسم الفرع يجب أن يكون محارف ',
-            'branch_id.max'          => 'رمز الفرع الرئيسي تجاوز 50 رقم ',
-            'branch_id.numeric'      => 'رمز الفرع الرئيسي يجب أن يكون ارقام  ',
-            'responsibility.max'     => 'معلومات الفرع(الصفة) تجاوز 250 محرف ',
-            'responsibility.string'  => 'معلومات الفرع يجب أن يكون محارف ',
-            'address.max'            =>  'عنوان الفرع تجاوز 100 محرف ',
-            'address.string'         =>'عنوان الفرع يجب أن يكون محارف ',
-            'website.unique'         =>'الموقع الالكتروني مُدخل سابقاً لفرع آخر ',
-            'website.string'         =>'الموقع الالكتروني يجب أن يكون محارف  ',
-            'email.string'           =>' البريد الالكتروني  يجب أن يكون محارف ',
-            'email.email'            =>'ماتم إدخاله غير متوافق مع صيغة البريد الإلكتروني  ',
-            'email.unique'           =>'البريد الالكتروني مُدخل سابقاً لفرع آخر  ',
-            'phone.max'              => 'رقم الهاتف تجاوز 50 رقم ',
-            'phone.unique'           => 'رقم الهاتف مُدخل سابقاً لفرع آخر ',
-            'phone.string'           =>'رقم الهاتف يجب أن يكون محارف ',
-            'mobile.max'             => 'رقم الموبايل تجاوز 50 محرف ',
-            'mobile.unique'          => 'رقم الموبايل مُدخل سابقاً لفرع آخر ',
-            'mobile.string'          =>'رقم الموبايل يجب أن يكون محارف ',
-
-        ];
-    }
-
-    protected function getRules()
-    {
-        return $rules = [
-
-              'code'                   =>'required|string|unique:branches,code',
-              'name'                   =>'required|max:50|string|unique:branches,name' ,
-              'branch_id'              =>'max:50|numeric' ,
-              'responsibility'         =>'max:250|string',
-              'address'                =>'max:100|string',
-              'website'                =>'string|unique:branches,website',
-              'email'                  =>'string|email|unique:branches,email',
-              'phone'                  =>'max:50|string|unique:branches,phone',
-              'mobile'                 =>'max:50|string|unique:branches,mobile',
-        ];
-
+        return redirect()->route('branch.index');
     }
 }
