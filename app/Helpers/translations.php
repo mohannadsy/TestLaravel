65cfef0d30041ece5cad6ee8f8cd949392d1bab5
Nour Alkinj <noor22w2@gmail.com>
2022-07-30 23:24:10 +0300

    nour

--- app/Helpers/translations.php
+++ app/Helpers/translations.php
@@ -10,6 +10,10 @@ if (!function_exists('DummyFunction')) {
      */
     function translations($json)
     {
+        if(!file_exists($json)){
+            return [];
+        }
+        return json_decode(file_get_contents($json), true);
 
     }
 }
