3c7d19b9487c40cd320685c774c9ada42f54298b
Nour Alkinj <noor22w2@gmail.com>
2022-08-08 11:02:36 +0300

    nour

--- app/Http/Controllers/Branch/BranchController.php
+++ app/Http/Controllers/Branch/BranchController.php
@@ -186,7 +186,6 @@ class BranchController extends Controller
     public function store(StoreBranchRequest $request)
     {
         $id = Branch::orderBy('id', 'desc')->first()->id + 1;
-//        sleep(2);
         Branch::create($request->validated());
         $parameters = ['request' => $request, 'id' => $id];
         $this->callActivityMethod('store', $parameters);
