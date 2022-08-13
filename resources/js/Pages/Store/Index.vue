a9285c3088d116ea8a89ad67c8d2e31f5e230251
SaraAbdo96 <sara.nadim.abdo@gmail.com>
2022-08-13 12:14:26 -0700

    tree

--- resources/js/Pages/Store/Index.vue
+++ resources/js/Pages/Store/Index.vue
@@ -1,31 +1,29 @@
 <template>
-    <div class="row">
-        <div class="col-3">
-            <warehouse-directory></warehouse-directory>
-        </div>
-        <div class="col-9">
-            <currency-index></currency-index>
-        </div>
-
+  <div class="row">
+    <div class="col-3">
+      <warehouse-directory></warehouse-directory>
     </div>
-   
-
+    <div class="col-9">
+      <currency-index></currency-index>
+    </div>
+  </div>
 </template>
 
 <script>
-import Index from '../../components/currency/Index.vue'
-import WarehouseDirectory from '../../components/currency/WarehouseDirectory.vue'
+import Index from "../../components/currency/Index.vue";
+import WarehouseDirectory from "../../components/currency/WarehouseDirectory.vue";
 export default {
-    components: { 
-       currencyIndex: Index,
-        WarehouseDirectory,
-        
-        }
-}
+  components: {
+    currencyIndex: Index,
+    WarehouseDirectory,
+  },
+  props:{
+    stores:Array
+  }
+};
 </script>
 <style scoped>
-.a{
-     border-left-style: solid;
+.a {
+  border-left-style: solid;
 }
-
 </style>
