4fa73bf4f50aca817c94478abd1f101e272c8aaf
Nour Alkinj <noor22w2@gmail.com>
2022-08-13 12:41:24 +0300

    ,,,

---
+++ lang/en/store.php
@@ -0,0 +1,12 @@
+<?php
+return [
+
+     // Messages
+    'store show' => ' Store not Found',
+    'Root Store delete' => 'Root Store is not deleted',
+    'store delete' => 'it is not possible to delete a store that contains stores within it',
+
+
+
+
+];
