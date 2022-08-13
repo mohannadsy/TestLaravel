e568d8f5c71298a5ad5bdb1ed4993ad9401ecc59
SaraAbdo96 <sara.nadim.abdo@gmail.com>
2022-08-13 14:16:44 -0700

    store store

--- resources/js/components/store/MainInformation.vue
+++ resources/js/components/store/MainInformation.vue
@@ -1,73 +1,110 @@
 <template>
-    <div class="row px-3 pb-2">
-        <page-title><fa icon="warehouse" /> {{$t('warehouseCard')}}</page-title>     
-    </div>
-    <form>
-        <div class="row">
-            <div class="col-md-5">
-                <div class="form-group row mt-2">
-                    <elemet-label class="col-form-label col-md-4">{{$t('warehouseCode')}}</elemet-label>
-                    <div class="col-8">
-                        <element-input />
-                    </div>
-                </div>
-                 <div class="form-group row mt-2">
-                    <elemet-label class="col-form-label col-md-4">{{$t('warehouseName')}}</elemet-label>
-                    <div class="col-8">
-                        <element-input />
-                    </div>
-                </div>
-                 <div class="form-group row mt-2">
-                    <elemet-label class="col-form-label col-md-4">{{$t('latinName')}}</elemet-label>
-                    <div class="col-8">
-                        <element-input />
-                    </div>
-                </div>
-                
-            </div>
-            <div class="col-md-4">
-
-                 <div  class="toggle_container">
-                    <ToggleButton
-                    />
-                </div>
-
-                 <div class="form-group row mt-2"> 
-                    <elemet-label class="col-form-label col-md-4">{{$t('warehouseType')}}</elemet-label>
-                    <div class="col-8">
-                        <v-select />
-                    </div>
-                </div>
-                
-
-            </div>
+  <div class="row px-3 pb-2">
+    <page-title><fa icon="warehouse" /> {{ $t("warehouseCard") }}</page-title>
+  </div>
+  <form>
+    <div class="row">
+      <div class="col-md-5">
+        <div class="form-group row mt-2">
+          <elemet-label class="col-form-label col-md-4">{{
+            $t("warehouseCode")
+          }}</elemet-label>
+          <div class="col-8">
+            <element-input
+              @change="objChanged"
+              v-model.trim="storeObject.code"
+            />
+          </div>
         </div>
-    </form>
-    
+        <div class="form-group row mt-2">
+          <elemet-label class="col-form-label col-md-4">{{
+            $t("warehouseName")
+          }}</elemet-label>
+          <div class="col-8">
+            <element-input
+              @change="objChanged"
+              v-model.trim="storeObject.name"
+            />
+          </div>
+        </div>
+        <div class="form-group row mt-2">
+          <elemet-label class="col-form-label col-md-4">{{
+            $t("latinName")
+          }}</elemet-label>
+          <div class="col-8">
+            <element-input
+              @change="objChanged"
+              v-model.trim="storeObject.latin_name"
+            />
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="form-group row mt-2">
+          <elemet-label class="col-form-label col-md-4">{{
+            $t("warehouseType")
+          }}</elemet-label>
+          <div class="col-8">
+             <element-input
+              @change="objChanged"
+              v-model.trim="storeObject.type"
+            />
+          </div>
+        </div>
+        <div class="form-group row mt-2">
+          <elemet-label class="col-form-label col-md-4"
+            >الفرع الرئيسي</elemet-label
+          >
+          <div class="col-8">
+            <element-input
+              @change="objChanged"
+              v-model.trim="storeObject.branch_id"
+            />
+          </div>
+        </div>
 
+        <div class="toggle_container">
+          <ToggleButton v-model="storeObject.is_active" @change="objChanged" />
+        </div>
+      </div>
+    </div>
+  </form>
 </template>
 
 <script>
-import ElemetLabel from '../../Shared/ElemetLabel.vue';
-import ElementInput from '../../Shared/ElementInput.vue';
-import PageTitle from '../../Shared/PageTitle.vue';
-import VSelect from '../../Shared/VSelect.vue';
-import ToggleButton from '../../Shared/ToggleButton.vue';
-
+import ElemetLabel from "../../Shared/ElemetLabel.vue";
+import ElementInput from "../../Shared/ElementInput.vue";
+import PageTitle from "../../Shared/PageTitle.vue";
+import VSelect from "../../Shared/VSelect.vue";
+import ToggleButton from "../../Shared/ToggleButton.vue";
+import { useForm } from "@inertiajs/inertia-vue3";
 export default {
-    components:{
-        ElemetLabel,
-        ElementInput,
-        PageTitle,
-        VSelect,
-        ToggleButton
-    }
-    
-}
+  components: {
+    ElemetLabel,
+    ElementInput,
+    PageTitle,
+    VSelect,
+    ToggleButton,
+  },
+  data() {
+    return {
+      storeObject: useForm({
+        code: "",
+        name: "",
+        latin_name: "",
+        type: "",
+        is_active: true,
+        branch_id: "",
+      }),
+    };
+  },
+  computed: {
+    objChanged() {
+      this.$emit("save-main", this.storeObject);
+    },
+  },
+};
 </script>
 
 <style scoped>
-
-
-
 </style>
---
+++ resources/js/store/modules/stores/mutation.js
@@ -0,0 +1,5 @@
+export default {
+    newStore(state, payload) {
+        state.stores.push(payload);
+    },
+};
