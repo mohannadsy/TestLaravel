ee2de38591759eb2d78799fa16198faa057e3aaa
Clauda <klodaalrakkad@gmail.com>
2022-08-14 14:18:26 +0300

    back

--- app/Http/Requests/StoreStoreRequest.php
+++ app/Http/Requests/StoreStoreRequest.php
@@ -15,8 +15,8 @@ class StoreStoreRequest extends FormRequest
     public function rules()
     {
         return [
-            'code'                   =>'required|string|unique:branches,code',
-            'name'                   =>'required|max:50|string|unique:branches,name' ,
+            'code'                   =>'required|string|unique:stores,code',
+            'name'                   =>'required|max:50|string|unique:stores,name' ,
 //          'branch_id'              =>'max:50|numeric' ,
 //          'responsibility'         =>'max:250|string',
 //          'address'                =>'max:100|string',
