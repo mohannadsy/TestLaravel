7644128fc57827e24993b482a13d517d39fd61ac
Clauda <klodaalrakkad@gmail.com>
2022-08-14 09:33:08 +0300

    back

--- lang/ar/store.php
+++ lang/ar/store.php
@@ -3,9 +3,9 @@
 return [
 
      // Messages
-    'store not found' => '  المستودع غير موجود ',
+    'store not found' => ' المستودع غير موجود ',
     'Root Store can not be deleted' => 'المستودع  الرئيسي لايحذف ',
-    'store delete error' => ' غير ممكن حذف هذا المستودع لأنه يحوي مستودعات ضمنية ',
+    'store delete error' => ' لا يمكن حذف هذا المستودع لأنه يحوي مستودعات ضمنية ',
 
 
 ];
