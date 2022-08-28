6226409e66e51199aaad0726722b879a56ab2edd
rgdrgd <raghad.nanou@gmail.com>
2022-08-28 13:17:04 +0300

    rgd

--- resources/js/components/store/CumulativeAcountType.vue
+++ resources/js/components/store/CumulativeAcountType.vue
@@ -2,23 +2,25 @@
     <div class="py-2">
         <table>
             <tr>
-                <!-- <th colspan="2"></th> -->
                 <th colspan="2">المستودعات</th>
             </tr>
             <tr>
                 <td class="num">الرقم</td>
                 <td class="store">المستودع</td>
             </tr>
-            <tr v-for="(store,i) in numOfRows" :key="i">
-                <td>{{ i++ }}</td>
-                <td><element-input v-model="store.storename"></element-input> </td>
+            <tr v-for="(numOfRow,i) in numOfRows" :key="i">
+                <td>{{ ++i }}</td>
+                <td><element-input v-model="numOfRow.storename"></element-input> </td>
             </tr>
-            <!-- <tr>
-                <td></td>
-                <td><button type="button" @click="removeRow(row)">delete</button></td>
-            </tr> -->
         </table>
-        <button @click="addRow">add row</button>
+            <button type='button' class="btn btn-info" @click="addRow">
+                <fa icon="plus-circle" />
+                    Add
+            </button>
+              <button type='button' class="btn btn-info" @click="deleteRow(i, numOfRow)">
+                
+                    delete
+            </button>
     </div>
 </template>
 
@@ -30,22 +32,27 @@ export default {
     },
     data() {
         return {
-            store:[{storename:''}],
+            numOfRows:[{storename:''}],
             numOfRows:3,
            newNumOfRows:'',
         };
     },
     methods:{
     addRow: function(){
-    //   this.rows.push({});
-      newNumOfRows= this.numOfRows++,
-      numOfRows = newNumOfRows
-        // newNumOfRows = this.numOfRows++;
-
+    //   newNumOfRows= this.numOfRows++;
+    //   numOfRows = newNumOfRows;
+     this.numOfRows++;
 
     },
-     addRow: function() {      
-       this.users.push({storename:'',})}
+    deleteRow (index , store){
+          var idx = this.numOfRows.indexOf(numOfRow);
+            console.log(idx, index);
+            if (idx > -1) {
+                this.store.splice(idx, 1);
+            }
+    },
+     add: function() {      
+       this.store.push({storename:'',})}
     // removeRow: function(row){
     //   //console.log(row);
     //   this.rows.$remove(row);
